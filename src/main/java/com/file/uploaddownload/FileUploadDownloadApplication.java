cc77223ae60458c4de92183f4a4581b25bcb7b17
Dhrutimaya <dhrutimaya.moharana@rapidsoft.co.in>
2023-05-02 13:15:26 +0530

    @Commit

--- src/main/java/com/file/uploaddownload/FileUploadDownloadApplication.java
+++ src/main/java/com/file/uploaddownload/FileUploadDownloadApplication.java
@@ -3,7 +3,11 @@ package com.file.uploaddownload;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+@EnableEurekaClient
 @SpringBootApplication
 public class FileUploadDownloadApplication extends SpringBootServletInitializer{
 
